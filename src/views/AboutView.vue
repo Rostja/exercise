7e8a55b43b27dc3b62fbb1556d1fc64f0f3682fa
Rostja <rasto.dorcak@gmail.com>
2024-09-08 11:23:20 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is page about Rasto</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat iusto cumque deleniti fuga
+      quos perferendis, commodi dolore, ipsum, sed doloremque consectetur earum exercitationem
+      minima quidem at sint eos ex molestias?
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
